490f43fb2e0423ba22171b9c4c6b9429ee3638c8
Romeo Flores <roflores@mit.edu>
2018-03-28 21:07:44 -0400

    added FAQs and team

--- app.js
+++ app.js
@@ -44,4 +44,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// port config
+const port = process.env.PORT || 3000; // config variable
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET FAQs page. */
+router.get('/faqs', function(req, res, next) {
+  res.render('faqs', { title: 'FAQs' });
+});
+
+/* GET Conact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
 });
 
 module.exports = router;
